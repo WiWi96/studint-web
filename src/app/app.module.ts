714266391a7e1adcda200521c7704ed3e36cc5f5
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-08-26 22:53:05 +0200

    Added homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'ng5-auto-complete';
 import { CommonModule } from '@angular/common';
 
@@ -10,11 +10,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 // Services
-import { UserService } from '../_service/profile/user/user.service';
-import { CompanyService } from '../_service/profile/company/company.service';
-import { UniversityService } from '../_service/profile/university/university.service';
-import { MainPageService } from '../_service/mainpage/mainpage.service';
+import { UserService } from '_service/profile/user/user.service';
+import { CompanyService } from '_service/profile/company/company.service';
+import { UniversityService } from '_service/profile/university/university.service';
+import { MainPageService } from '_service/mainpage/mainpage.service';
 // Site components
 import { NavigationBarComponent } from '../_components/navigation-bar/navigation-bar.component';
 import { LoginComponent } from '../_components/login';
@@ -24,10 +25,12 @@ import { UniversityProfileComponent} from '../_components/university-profile';
 import { CompanyProfileComponent} from '../_components/company-profile';
 import { StaticBarComponent } from '../_components/static-bar/static-bar.component';
 import { WallComponent } from '../_components/wall';
+import { HomeComponent } from '_components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavigationBarComponent,
     LoginComponent,
     RegisterComponent,
@@ -45,6 +48,7 @@ import { WallComponent } from '../_components/wall';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
+    AngularSvgIconModule,
     NgbModule.forRoot()
   ],
   providers: [UserService, CompanyService, UniversityService, MainPageService],
