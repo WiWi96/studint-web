8612418c5910b075660d49832744963b4c356eb5
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-08-27 23:33:25 +0200

    Added project profile, moment.js, ngx-moment libraries

--- src/_models/profile/projectProfile.ts
+++ src/_models/profile/projectProfile.ts
@@ -4,16 +4,14 @@ import { Team } from '../team/team';
 import { Duration } from '../duration';
 
 export class ProjectProfile {
-    id: number;
     profileName: ProfileName;
-    name: string;
     description: string;
     technologies: Array<Skill> = [];
     type: string;
     level: string;
     participants: Array<ProfileName> = [];
-    teams: Array<Team> = [];
-    participationStatus: string;
+    //teams: Array<Team> = [];
+    //participationStatus: string;
     startDate: Date;
     joiningDate: Date;
     duration: Duration;
