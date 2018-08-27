8085b0668a254923c0ef0d6447079d929295c890
Adam Pałka <adampalka1@op.pl>
2018-08-27 23:07:17 +0200

    data from obj ,mobile v impove, proj and uni showing add

--- src/_components/static-bar/static-bar.component.ts
+++ src/_components/static-bar/static-bar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileName } from '_models/profile/profileName';
+import { Skill } from '_models/skill/skill';
+import { Team } from '_models/team/team';
+import { Duration } from '_models/duration';
+import { ProjectProfile } from '_models/profile/projectProfile';
+import { UniversityProfile } from '_models/profile/universityProfile';
+
 
 @Component({
   selector: 'app-static-bar',
@@ -7,22 +14,200 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaticBarComponent implements OnInit {
   showMenu = false;
-  public projects: Array<string>;
-  public showProjects: boolean;
+  showProjects = false;
+  showUniversities = false;
+  projects: Array<ProjectProfile>;
+  universities: Array<UniversityProfile>;
+
 
   constructor() {
-    this.showProjects = false;
-    this.projects = ['item1', 'item2', 'item3'];
+    // temp projects initialization
+    this.projects = [
+      {
+        id: 1,
+        name: 'Project 1',
+        profileName: {
+          id: 1,
+          name: 'Project 1',
+          photo: ''
+        },
+        description: 'string',
+        technologies: [new Skill],
+        type: 'string',
+        level: 'string',
+        participants: [new ProfileName],
+        teams: [new Team],
+        participationStatus: 'string',
+        startDate: new Date,
+        joiningDate: new Date,
+        duration: new Duration,
+        status: 'string',
+      },
+      {
+        id: 2,
+        name: 'Project 2',
+        profileName: {
+          id: 2,
+          name: 'Project 2',
+          photo: ''
+        },
+        description: 'string',
+        technologies: [new Skill],
+        type: 'string',
+        level: 'string',
+        participants: [new ProfileName],
+        teams: [new Team],
+        participationStatus: 'string',
+        startDate: new Date,
+        joiningDate: new Date,
+        duration: new Duration,
+        status: 'string',
+      },
+      {
+        id: 3,
+        name: 'Project 3',
+        profileName: {
+          id: 3,
+          name: 'Project 3',
+          photo: ''
+        },
+        description: 'string',
+        technologies: [new Skill],
+        type: 'string',
+        level: 'string',
+        participants: [new ProfileName],
+        teams: [new Team],
+        participationStatus: 'string',
+        startDate: new Date,
+        joiningDate: new Date,
+        duration: new Duration,
+        status: 'string',
+      },
+    ];
+
+    // temp universities initialization
+    this.universities = [
+      {
+        profileName: {
+            id: 1,
+            name: 'Silesian University of Technology',
+            photo: ''
+        },
+        description: '',
+        profiles: new Map(),
+        posts: [
+            {
+                id: 1,
+                author: {
+                    id: 1,
+                    name: '',
+                    photo: '',
+                },
+                publishedDate: new Date(),
+                post: ''
+            },
+        ],
+        address: {
+            town: '',
+            postCode: '',
+            street: '',
+            country: '',
+            houseNo: 2,
+        },
+        type: '',
+        courses: [
+            {
+                id: 1,
+                name: ''
+            },
+        ],
+      },
+      {
+        profileName: {
+            id: 2,
+            name: 'Akademia Górniczo Hutnicza w Krakowie',
+            photo: ''
+        },
+        description: '',
+        profiles: new Map(),
+        posts: [
+            {
+                id: 1,
+                author: {
+                    id: 1,
+                    name: '',
+                    photo: '',
+                },
+                publishedDate: new Date(),
+                post: ''
+            },
+        ],
+        address: {
+            town: '',
+            postCode: '',
+            street: '',
+            country: '',
+            houseNo: 2,
+        },
+        type: '',
+        courses: [
+            {
+                id: 1,
+                name: ''
+            },
+        ],
+      },
+      {
+        profileName: {
+            id: 3,
+            name: 'Harvard University',
+            photo: ''
+        },
+        description: '',
+        profiles: new Map(),
+        posts: [
+            {
+                id: 1,
+                author: {
+                    id: 1,
+                    name: '',
+                    photo: '',
+                },
+                publishedDate: new Date(),
+                post: ''
+            },
+        ],
+        address: {
+            town: '',
+            postCode: '',
+            street: '',
+            country: '',
+            houseNo: 2,
+        },
+        type: '',
+        courses: [
+            {
+                id: 1,
+                name: ''
+            },
+        ],
+      },
+    ];
+
   }
 
   ngOnInit() {
   }
 
-  public toggleProjects() {
+  toggleProjects() {
     this.showProjects = !this.showProjects;
   }
 
-  public closeNav() {
+  toggleUniversities() {
+    this.showUniversities = !this.showUniversities;
+  }
+
+  closeNav() {
     // document.getElementById('mySidenav').style.width = '0';
     this.showMenu = false;
   }
