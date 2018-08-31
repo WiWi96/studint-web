faaebf4b6431f0d3fea190755a697ec322c7955f
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-08-31 16:19:17 +0200

    Changed error component behaviour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,19 @@ import { UniversityProfileComponent } from '_components/university-profile';
 import { CompanyProfileComponent } from '_components/company-profile';
 import { ProjectProfileComponent } from '_components/project-profile';
 import { HomeComponent } from '_components/home/home.component';
-import { ErrorPageComponent } from '_components/error-page/error-page.component';
+import { ErrorsComponent } from '_components/error-page/error-page.component';
 import { AddressFormComponent } from '_components/_forms/address-form/address-form.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent},
-    { path: 'address', component: AddressFormComponent},
-    { path: 'user/:id', component: UserProfileComponent},
-    { path: 'university/:id', component: UniversityProfileComponent},
-    { path: 'project/:id', component: ProjectProfileComponent},
-    { path: 'company/:id', component: CompanyProfileComponent},
-    { path: '**', component: ErrorPageComponent}
+    { path: 'register', component: RegisterComponent },
+    { path: 'address', component: AddressFormComponent },
+    { path: 'user/:id', component: UserProfileComponent },
+    { path: 'university/:id', component: UniversityProfileComponent },
+    { path: 'project/:id', component: ProjectProfileComponent },
+    { path: 'company/:id', component: CompanyProfileComponent },
+    { path: '**', component: ErrorsComponent, data: { code: 404 } }
 ];
 
 // export const routing = RouterModule.forRoot(routes);
