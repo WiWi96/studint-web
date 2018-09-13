e0780dd4a24f1f0aa18cad5be7f89c16c0acfdf4
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-09-13 22:50:11 +0200

    Fixed wall component, added routing links according to profile type, changed input parameters

--- src/_components/_posts/wall/wall.component.ts
+++ src/_components/_posts/wall/wall.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Post } from '_models/post';
 import { ProfileName } from '_models/profile/profileName';
 import { PostService } from '_service/post/post.service';
@@ -10,13 +10,31 @@ import { UtilsService } from '_service/utils/utils.service';
     templateUrl: 'wall.component.html',
     styleUrls: ['./wall.component.less'],
 })
-export class WallComponent implements OnInit {
-    @Input() posts: Array<Post>;
+export class WallComponent implements OnInit, OnDestroy {
+    @Input() profile: ProfileName;
+    posts: Array<Post>;
+    sub: any;
+
     constructor(
         private utils: UtilsService,
         private postService: PostService,
-        private mainPageService: MainPageService 
+        private mainPageService: MainPageService
     ) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        if (!this.profile) {
+            this.sub = this.postService.getAllPosts().subscribe(
+                data => this.posts = data,
+                err => console.log(err));
+        }
+        else {
+            this.sub = this.postService.getPostsByProfile(this.profile.id).subscribe(
+                data => this.posts = data,
+                err => console.log(err));
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 }
