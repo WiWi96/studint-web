53ae5d4d0b1d7df8e0e1cb1937e5c02055550ecd
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-09-22 01:01:19 +0200

    Fixed going to user profile

--- src/_components/navigation-bar/navigation-bar.component.ts
+++ src/_components/navigation-bar/navigation-bar.component.ts
@@ -8,106 +8,100 @@ import { ProfileName } from '_models/profile/profileName';
 import { UserType } from '_enums/userType';
 import { UtilsService } from '_service/utils/utils.service';
 @Component({
-  selector: 'app-navigation-bar',
-  templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.less'],
+    selector: 'app-navigation-bar',
+    templateUrl: './navigation-bar.component.html',
+    styleUrls: ['./navigation-bar.component.less'],
 })
 export class NavigationBarComponent implements OnInit {
-  isCollapsed = true;
-  smallScreen: boolean;
-  loggedUser: ProfileName;
+    isCollapsed = true;
+    smallScreen: boolean;
 
-  selected: string;
-  states: string[] = [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Dakota',
-    'North Carolina',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming'
-  ];
+    selected: string;
+    states: string[] = [
+        'Alabama',
+        'Alaska',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'Florida',
+        'Georgia',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Dakota',
+        'North Carolina',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming'
+    ];
 
-  search = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(term => term.length < 2 ? []
-        : this.states.filter(v => v.toLowerCase().includes(term.toLocaleLowerCase())).splice(0, 10))
-    )
-  constructor(private authService: AuthService,
-    private router: Router,
-    private utils: UtilsService) {
-  }
-
-  ngOnInit() {
-    this.onResize();
-    this.authService.getLoggedProfile().subscribe(
-      data => this.loggedUser = data
-    )
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    this.smallScreen = window.innerWidth < 992;
-  }
-
-  goToProfile() {
-    if (this.loggedUser) {
-      this.router.navigate([this.utils.getUserRouterLink(this.loggedUser.type), this.loggedUser.id]);
+    search = (text$: Observable<string>) =>
+        text$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            map(term => term.length < 2 ? []
+                : this.states.filter(v => v.toLowerCase().includes(term.toLocaleLowerCase())).splice(0, 10))
+        )
+    constructor(private authService: AuthService,
+        private router: Router,
+        private utils: UtilsService) {
     }
-  }
 
-  goToSettings() {
+    ngOnInit() {
+        this.onResize();
+    }
 
-  }
+    @HostListener('window:resize')
+    onResize() {
+        this.smallScreen = window.innerWidth < 992;
+    }
 
-  signOut() {
-    this.authService.logOut();
-    this.router.navigate(['/']);
-  }
+    goToProfile() {
+        this.authService.goToUserProfile();
+    }
+
+    goToSettings() {
+
+    }
+
+    signOut() {
+        this.authService.logOut();
+        this.router.navigate(['/']);
+    }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,73 +7,89 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { ReplaySubject } from '../../../node_modules/rxjs/ReplaySubject';
 import { environment } from '../../environments/environment';
 import { ProfileName } from '_models/profile/profileName';
+import { UtilsService } from '_service/utils/utils.service';
 
 export interface JwtToken {
-  token: string;
-  type: string;
+    token: string;
+    type: string;
 }
 
 @Injectable()
 export class AuthService {
-  jwtService = new JwtHelperService();
-  permissionChanged: ReplaySubject<boolean> = new ReplaySubject<boolean>();
+    jwtService = new JwtHelperService();
+    permissionChanged: ReplaySubject<boolean> = new ReplaySubject<boolean>();
+    loggedUser: ProfileName;
+    sub: any;
 
-  constructor(private storage: TokenStorage, private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+    constructor(private storage: TokenStorage, private http: HttpClient, private router: Router, private route: ActivatedRoute, private utils: UtilsService) { }
 
-  isLogged(): boolean {
-    let token = this.storage.getToken();
-    return token != null && !this.jwtService.isTokenExpired(token);
-  }
-
-  logIn(username: string, password: string): Observable<any> {
-    return this.http.post(`${environment.apiEndpoint}/signin`, { username: username, password: password });
-  }
-
-  storeToken(token: JwtToken) {
-    this.storage.saveToken(token.token);
-  }
-
-  logOut() {
-    this.storage.signOut();
-  }
-
-  getRoles(): string[] {
-    let result: string[] = null;
-    try {
-      const token = this.jwtService.decodeToken(this.storage.getToken());
-      result = token.scopes != null ? token.scopes.split(',') : null;
-    } catch (error) {
+    isLogged(): boolean {
+        let token = this.storage.getToken();
+        return token != null && !this.jwtService.isTokenExpired(token);
     }
-    return result;
-  }
 
-  getUser(): string {
-    let result: string = null;
-    try {
-      const token = this.jwtService.decodeToken(this.storage.getToken());
-      result = token.sub;
-    } catch (error) {
+    logIn(username: string, password: string): Observable<any> {
+        return this.http.post(`${environment.apiEndpoint}/signin`, { username: username, password: password });
     }
-    return result;
-  }
 
-  isAdmin(): boolean {
-    const roles: string[] = this.getRoles();
-    return roles != null && roles.includes('ADMIN');
-  }
-
-  isManager(): boolean {
-    const roles: string[] = this.getRoles();
-    return roles != null && roles.includes('USER');
-  }
-
-  refreshPermissions() {
-    this.permissionChanged.next(true);
-  }
-
-  public getLoggedProfile(): Observable<ProfileName> {
-    if (this.isLogged()) {
-      return this.http.get<ProfileName>(`${environment.apiEndpoint}/profile`);
+    storeToken(token: JwtToken) {
+        this.storage.saveToken(token.token);
     }
-  }
+
+    logOut() {
+        this.storage.signOut();
+        this.loggedUser = null;
+        this.sub.unsubscribe();
+    }
+
+    getRoles(): string[] {
+        let result: string[] = null;
+        try {
+            const token = this.jwtService.decodeToken(this.storage.getToken());
+            result = token.scopes != null ? token.scopes.split(',') : null;
+        } catch (error) {
+        }
+        return result;
+    }
+
+    getUser(): string {
+        let result: string = null;
+        try {
+            const token = this.jwtService.decodeToken(this.storage.getToken());
+            result = token.sub;
+        } catch (error) {
+        }
+        return result;
+    }
+
+    fetchUserProfileName() {
+        this.sub = this.getLoggedProfile().subscribe(
+            data => this.loggedUser = data,
+            (error) => {console.log(error)}
+        )
+    }
+
+    isAdmin(): boolean {
+        const roles: string[] = this.getRoles();
+        return roles != null && roles.includes('ADMIN');
+    }
+
+    isManager(): boolean {
+        const roles: string[] = this.getRoles();
+        return roles != null && roles.includes('USER');
+    }
+
+    refreshPermissions() {
+        this.permissionChanged.next(true);
+    }
+
+    public getLoggedProfile(): Observable<ProfileName> {
+        return this.http.get<ProfileName>(`${environment.apiEndpoint}/profile`);
+    }
+
+    public goToUserProfile() {
+        if (this.loggedUser) {
+            this.router.navigate([this.utils.getUserRouterLink(this.loggedUser.type), this.loggedUser.id]);
+        }
+    }
 }
