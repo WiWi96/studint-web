9d5f19f204e38a7b13b17543762cc65a1fcd3af9
Dawid Skomorochow <Dawid Skomorochow@DESKTOP-IUOJS5F>
2018-09-24 07:21:09 +0200

    added few things

--- src/_components/_forms/student-edit-modal/student-edit-modal.component.ts
+++ src/_components/_forms/student-edit-modal/student-edit-modal.component.ts
@@ -12,6 +12,8 @@ import { LanguageService } from '_service/language/language.service';
 import { TypeaheadMatch } from 'ngx-bootstrap';
 import { Skill } from '_models/skill/skill';
 import { SkillService } from '_service/skill/skill.service';
+import { ServiceType } from '_enums/serviceTypes';
+import { SocialMedia } from '_models/socialMedia';
 
 @Component({
   selector: 'app-student-edit-modal',
@@ -37,8 +39,12 @@ export class StudentEditModalComponent implements OnInit {
   skills: Skill[];
   // submitted student
   submittedStudent: boolean = false;
+  //social Services
+  socialServices: string[];
+  socialServicesFormpGroup: FormGroup;
 
   user: UserProfile;
+  socialMediaMap: Map<ServiceType, string> = new Map<ServiceType, string>();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -55,6 +61,7 @@ export class StudentEditModalComponent implements OnInit {
     this.userSkillTags = JSON.parse(JSON.stringify(this.user.skills));
     this.getLanguages();
     this.getTechnologies();
+    this.getStudentSocialMedia();
     this.createStudentForm();
   }
 
@@ -65,9 +72,11 @@ export class StudentEditModalComponent implements OnInit {
       return;
 
     this.user.description = this.accountStudentDetailsFormGroup.get('description').value; // Add 1
+    this.user.profileName.name = this.fullNameFormGroup.get('firstName').value + " " + this.fullNameFormGroup.get('surname').value;
   
     this.user.skills = this.userSkillTags;
     this.user.languages = this.userLanguagesTags ;
+    this.setStudentSocialServices();
     this.userProfileService.updateUser(this.user).subscribe();
     this.activeModal.dismiss();
   }
@@ -88,8 +97,8 @@ export class StudentEditModalComponent implements OnInit {
 
     var firstWord = totalWords.replace(/ .*/,'');
     var secondWord = totalWord2.replace(/(([\.\?\!]|^)\s*\b\w+)/gm,"$2");
-    
-    console.log(secondWord);
+    secondWord = secondWord.replace(/^[ ]+|[ ]+$/g,'')
+
 
     this.fullNameFormGroup = this.formBuilder.group({
       firstName: [firstWord, [Validators.required, Validators.pattern("[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż ]+")]],
@@ -98,11 +107,24 @@ export class StudentEditModalComponent implements OnInit {
   }
 
   createAccountDetailsFormGroup() {
+    this.socialServicesFormpGroup = this.formBuilder.group({
+      youtube: [this.socialMediaMap.get(ServiceType.YouTube)],
+      facebook: [this.socialMediaMap.get(ServiceType.Facebook)],
+      twitter: [this.socialMediaMap.get(ServiceType.Twitter)],
+      instagram: [this.socialMediaMap.get(ServiceType.Instagram)],
+      linkedin: [this.socialMediaMap.get(ServiceType.LinkedIn)],
+      github: [this.socialMediaMap.get(ServiceType.GitHub)],
+      pinterest: [this.socialMediaMap.get(ServiceType.Pinterest)],
+      google: [this.socialMediaMap.get(ServiceType.Google)],
+      custom: [this.socialMediaMap.get(ServiceType.Custom)]
+    });
+
     this.accountStudentDetailsFormGroup = this.formBuilder.group({
       fullname: this.fullNameFormGroup,
       language: this.languageFormControl,
       technology: this.technologyFormControl,
-      description: [this.user.description]
+      description: [this.user.description],
+      socialServices: this.socialServicesFormpGroup,
     });
   }
 
@@ -117,6 +139,36 @@ export class StudentEditModalComponent implements OnInit {
       this.languages = languages;
     })
   }
+
+  getStudentSocialMedia() {
+    this.user.profiles.forEach(social => {
+      this.socialMediaMap.set(social.service, social.url);
+    })
+  }
+
+  setStudentSocialServices() {
+    this.user.profiles = [];
+
+    if (this.socialServicesFormpGroup.get('youtube').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.YouTube, this.socialServicesFormpGroup.get('youtube').value));
+    if (this.socialServicesFormpGroup.get('facebook').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Facebook, this.socialServicesFormpGroup.get('facebook').value));
+    if (this.socialServicesFormpGroup.get('twitter').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Twitter, this.socialServicesFormpGroup.get('twitter').value));
+    if (this.socialServicesFormpGroup.get('instagram').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Instagram, this.socialServicesFormpGroup.get('instagram').value));
+    if (this.socialServicesFormpGroup.get('linkedin').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.LinkedIn, this.socialServicesFormpGroup.get('linkedin').value));
+    if (this.socialServicesFormpGroup.get('github').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.GitHub, this.socialServicesFormpGroup.get('github').value));
+    if (this.socialServicesFormpGroup.get('pinterest').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Pinterest, this.socialServicesFormpGroup.get('pinterest').value));
+    if (this.socialServicesFormpGroup.get('google').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Google, this.socialServicesFormpGroup.get('google').value));
+    if (this.socialServicesFormpGroup.get('custom').value)
+      this.user.profiles.push(new SocialMedia(ServiceType.Custom, this.socialServicesFormpGroup.get('custom').value));
+  }
+
 
   public onTechnologyCloseClick(skillTag: Skill): void {
     this.userSkillTags.forEach((skill, index) => {
